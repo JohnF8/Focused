f4658a8af6f2187aa183e26171c460b85d80e24c
John <johnnf8@gmail.com>
2017-10-07 09:05:54 -0400

    Added the rest of the project files and added functionality for JSON Parsing

--- app/src/main/java/com/example/john/focused/TestHttp.java
+++ app/src/main/java/com/example/john/focused/TestHttp.java
@@ -2,6 +2,7 @@ package com.example.john.focused;
 
 import android.app.DownloadManager;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,17 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-
 
 
 public class TestHttp extends AppCompatActivity {
@@ -33,8 +30,66 @@ public class TestHttp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_http);
-         text = (TextView) findViewById(R.id.displayTest);
+        text = (TextView) findViewById(R.id.displayTest);
         btn = (Button) findViewById(R.id.sendTest);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new JSONTask().execute("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesDemoItem.txt");
+            }
+        });
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... params){
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try{
+                URL url = new URL(params[0]);
+                connection=(HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line="";
+
+                while((line=reader.readLine()) != null){
+                    buffer.append(line);
+                }
+                return buffer.toString();
+            }
+
+            catch(MalformedURLException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            finally {
+                if(connection != null){
+                    connection.disconnect();
+                }
+                try{
+                    if(reader != null){
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            text.setText(result);
+        }
     }
 
 
